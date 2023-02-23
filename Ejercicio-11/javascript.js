95499deff90da3454353ed4d96379d3951cd4ddc
Pedro Vilamitjana <pepevila98.pv@gmail.com>
2023-02-23 03:13:11 -0300

    intento de generar un solo boton para que cambie los tres estilos

--- Ejercicio-11/javascript.js
+++ Ejercicio-11/javascript.js
@@ -1,3 +1,19 @@
-function cambiarEstilo(hojaDeEstilo){
-    document.getElementById('paginaDeEstilo').setAttribute('href', hojaDeEstilo);
-}
+// function cambiarEstilo(hojaDeEstilo) {
+//     document
+//         .getElementById("paginaDeEstilo")
+//         .setAttribute("href", hojaDeEstilo);
+// }
+
+const cambiarEstilo = document.querySelector(".estilo");
+const boton = document.querySelector("#botonCambiar");
+console.log(cambiarEstilo);
+boton.addEventListener("click", () => {
+    if (cambiarEstilo.href === "http://127.0.0.1:5500/Ejercicio-11/") {
+        cambiarEstilo.href = "/css/estilos-retro.css";
+    } else if (cambiarEstilo.href === "/css/estilos-retro.css") {
+        cambiarEstilo.href = "/css/estilos-futuro.css";
+    } else {
+        cambiarEstilo.href = "/css/estilos.css";
+    }
+}); //arrow function
+console.log(cambiarEstilo);
